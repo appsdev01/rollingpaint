8abb5946705fb14dd2518c779604e434426d3d08
appsdev01 <apps.dev01@yopmail.com>
2015-06-15 10:27:51 +0900

    Edit room page.

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic.contrib.froste
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
