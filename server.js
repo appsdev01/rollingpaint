9c3bdfc1d23a8afbde224ddcac8a09730d82917b
appsdev01 <apps.dev01@yopmail.com>
2015-08-04 11:31:27 +0900

    Add pictures api.

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express'),
     app = express();
-
 var scores = require('./routes/scores');
 var users = require('./routes/users');
 var chats = require('./routes/chats');
 var rooms = require('./routes/rooms');
+var paints = require('./routes/pictures');
 var bodyParser = require('body-parser');
 
 app.use(express.static('www'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.use('/scores', scores);
 app.use('/users', users);
 app.use('/chats', chats);
 app.use('/rooms', rooms);
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use('/pictures', paints);
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
