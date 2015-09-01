602c546734278f7a40042b137cef05444890df17
helloham <helloham.seo@samsung.com>
2015-09-01 18:41:34 +0900

    design B/E API ver.2 (game del), change room component to lobby

--- server/models/room.js
+++ server/models/room.js
@@ -2,21 +2,22 @@ var mongoose = require('mongoose');
 var ShortId = require('mongoose-minid');
 var Schema = mongoose.Schema;
 
-var gameUserSchema = new Schema({
-  userId: ShortId,
-  word: String,
-  score: Number
-});
+// var gameUserSchema = new Schema({
+//   userId: ShortId,
+//   ready: Boolean, // true:ready, false:not ready
+// });
 
 var roomSchema = new Schema({
   _id: ShortId,
   title: String,
-  password: String,
+  password: String, // null default
   capacity: Number,
   ownerId: String,
-  status: String, // 01: opened, 02: playing, 03: ended
-  step: Number,
-  users: [gameUserSchema],
+  status: String, // 01: opened(default), 02: playing, 03: ended
+  wordseed: String, // random value
+  gameround: Number, // 1(default)
+  users: [ShortId], // ownerId default
+  sketchbooks: [ShortId], // status(02) > game
   date: { type: Date, default: Date.now }
 });
 
---
+++ www/app/lobby/lobby.js
@@ -0,0 +1,67 @@
+angular.module('lobby', ['ionic'])
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('lobby', {
+        url: '/lobby',
+        templateUrl: "app/lobby/lobby.html",
+        controller: 'LobbyCtrl'
+      });
+  })
+  .controller('LobbyCtrl', function($scope, $http, $ionicModal) {
+
+    $http.get('/rooms').then(function(response) {
+      console.log(response);
+      $scope.rooms = response.data;
+    });
+
+    $scope.roomData = {
+      // "title": "",
+      // "password": ""
+      // user 별 ready 상태 필드 추가
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.createRoom = function(req,res) {
+      //console.log('Doing login', $scope.loginData);
+      console.log('Create a Room', $scope.roomData);
+
+      $http({
+        method: 'POST',
+        url: 'rooms',
+        data: {
+          // "email": $scope.loginData.email,
+          // "password": $scope.loginData.password
+          "title": $scope.roomData.title,
+          "password": $scope.roomData.password,
+          "capaticy": $scope.roomData.capaticy,
+          "ownerId": "wBd9fbo", // 하드코딩 추후에 로그인 데이터 받아올 예정
+          "status": "01",
+          "wordseed": 0,
+          "gameround": 0,
+          "users": [{userId: "wBd9fbo"}],
+          "sketchbooks": ["a"]
+        }
+      }).success(function(response) {
+        if (response) {
+          console.log('Create a Room Success !!!');
+        }
+      });
+    };
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('app/lobby/newroom.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modalNewRoom = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeNewRoom = function() {
+      $scope.modalNewRoom.hide();
+    };
+
+    // Open the login modal
+    $scope.openNewRoom = function() {
+      $scope.modalNewRoom.show();
+    };
+  });
