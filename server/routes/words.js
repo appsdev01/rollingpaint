b15bc0f20649d453c56761266bdc604e9692caf6
sehee88 <lsh880910@naver.com>
2015-09-03 10:18:40 +0900

    Update suffling words

--- server/routes/words.js
+++ server/routes/words.js
@@ -14,21 +14,25 @@ router.post('/', function(req, res, next) {
 
   console.log(req.body);
   var seqNo = 0;
-  var word = new Word({ value: req.body.value });
+  var word = new Word({
+    value: req.body.value
+  });
   console.log("word : " + word);
-  word.save(function (err) {
+  word.save(function(err) {
     if (err) {
       return res.sendStatus(500);
     }
     Word.find(function(err, results) {
       console.log("results : " + results);
       seqNo = results.length;
-      Word.update(word, {seq: seqNo}, function(err, result) {
+      Word.update(word, {
+        seq: seqNo
+      }, function(err, result) {
         //  res.send(result);
       });
     });
 
-    Word.findById(word, function (err, doc) {
+    Word.findById(word, function(err, doc) {
       if (err) return handleError(err);
       res.send(doc);
     });
@@ -53,39 +57,41 @@ router.get('/wordList/:roomNo/users/:userSeq', function(req, res, next) {
   var userSeq = req.params.userSeq;
   var cardNum = 4;
   var seq = 0;
-  var wordListStr ="";
+  var wordListStr = "";
   var reset_array = [];
 
-  for(var i=0; i< 27; i ++){
-    shuffle_array[i] = i+1;
+  for (var i = 0; i < 27; i++) {
+    shuffle_array[i] = i + 1;
   }
   shuffle_array = shuffle(shuffle_array.slice(0), roomNum);
   console.log("===============================================");
   console.log("roomNum : " + roomNum + " after array : " + shuffle_array);
-  var fromSeq = cardNum * (userSeq-1);
-  var toSeq = (cardNum * (userSeq-1)) + (cardNum-1);
+  var fromSeq = cardNum * (userSeq - 1);
+  /*
+  var toSeq = (cardNum * (userSeq - 1)) + (cardNum - 1);
   console.log("from : " + fromSeq + " to : " + toSeq);
-
+  console.log("from : " + shuffle_array[fromSeq] + " to : " + shuffle_array[toSeq]);
+  */
   async.series([
-    function(callback){
-      Word.find(function(err, results) {
-        //console.log("results : " + results);
-        if(results !== null){
-          for(var k=0; k< cardNum; k ++){
-            reset_array[k] = results[fromSeq++].value;
+      function(callback) {
+        Word.find(function(err, results) {
+          //console.log("results : " + results);
+          if (results !== null) {
+            for (var k = 0; k < cardNum; k++) {
+              reset_array[k] = results[shuffle_array[fromSeq++]].value;
+            }
+            callback(null, reset_array);
+          } else {
+            callback(null, "No words");
           }
-          callback(null, reset_array);
-        } else{
-          callback(null, "No words");
-        }
-      });
-    }
-  ],
-  function(err, results){
-    if(!err){
-      res.send(results);
-    }
-  });
+        });
+      }
+    ],
+    function(err, results) {
+      if (!err) {
+        res.send(results);
+      }
+    });
 });
 
 
@@ -94,14 +100,20 @@ router.put('/:id', function(req, res, next) {
   if (!req.body) {
     return res.sendStatus(400);
   }
-  Word.update({_id: req.params.id}, {value: req.body.value}, function(err, result) {
+  Word.update({
+    _id: req.params.id
+  }, {
+    value: req.body.value
+  }, function(err, result) {
     res.send(result);
   });
 });
 
 // Delete a Word
 router.delete('/:id', function(req, res, next) {
-  Word.remove({_id: req.params.id}, function(err, result) {
+  Word.remove({
+    _id: req.params.id
+  }, function(err, result) {
     console.log(result);
     res.send(result);
   });
