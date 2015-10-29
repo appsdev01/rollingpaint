61d2b880ccbed452f21a1b3cca1b9de8ed6feaf5
appsdev01 <apps.dev01@yopmail.com>
2015-10-29 14:42:58 +0900

    Remove unused code.

--- www/js/app.js
+++ www/js/app.js
@@ -21,30 +21,6 @@ angular.module('starter', ['ionic', 'picture', 'ranking', 'guessword', 'word', '
     $ionicConfigProvider.views.maxCache(0);
     $urlRouterProvider.otherwise('/intro');
   })
-  // .factory('socket', function($rootScope) {
-  //   var socket = io.connect();
-  //   return {
-  //     on: function(eventName, callback) {
-  //       socket.on(eventName, function() {
-  //         var args = arguments;
-  //         $rootScope.$apply(function() {
-  //           callback.apply(socket, args);
-  //         });
-
-  //       });
-  //     },
-  //     emit: function(eventName, data, callback) {
-  //       socket.emit(eventName, data, function() {
-  //         var args = arguments;
-  //         $rootScope.$apply(function() {
-  //           if (callback) {
-  //             callback.apply(socket, args);
-  //           }
-  //         });
-  //       });
-  //     }
-  //   };
-  // })
   .factory('chatSocket', function($rootScope, $location) {
     var socketUrl = $location.protocol() + $location.host() + ':' + $location.port() + '/chat-socket';
     console.log(socketUrl);
