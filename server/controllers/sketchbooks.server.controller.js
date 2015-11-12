cd32d9f1a4b17cd34b0f8b07297362595ed9ddc4
sehee88 <lsh880910@naver.com>
2015-11-12 14:59:58 +0900

    Update Sketchbook Api

--- server/controllers/sketchbooks.server.controller.js
+++ server/controllers/sketchbooks.server.controller.js
@@ -160,7 +160,7 @@ exports.listPaper = function(req, res, next) {
     _id: req.params.sketchbookId
   }, function(err, results) {
     if (!err) {
-      console.log("results papers : " + results.papers[results.papers.length - 1].picture);
+      console.log("results papers : " + results.papers);
       res.send(results);
     }
   });
