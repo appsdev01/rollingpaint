c420d359257115a71c9fd864e0a0efa2c4c4e16d
helloham <helloham.seo@samsung.com>
2015-11-12 18:10:27 +0900

    B/E Room API Update(Room Delete)

--- www/app/room/room.js
+++ www/app/room/room.js
@@ -48,14 +48,36 @@ angular.module('room', ['ionic'])
         // 방 참가자 정보 조회
         var i = 1;
         angular.forEach($scope.room.players, function(player) {
-
           if (player.playStatus === '01') $scope.room.status = '01'; // 한 명이라도 ready 상태가 아니면 '01'
           $http.get('/users/' + player.userId).then(function(response) {
             $scope.players[response.data._id] = response.data;
             $scope.players[response.data._id].playStatus = player.playStatus;
             $scope.players[response.data._id].seq = i++;
-            });
+          });
         });
+
+        // 방장 위임하기
+        // console.log('★★★★★★★★★★★★★★★★★★★★★★★★★★★');
+        // console.log($scope.user._id);
+        // console.log($scope.room.ownerId);
+        // console.log($scope.room.players.length);
+        // console.log($scope.room.id);
+        // console.log('★★★★★★★★★★★★★★★★★★★★★★★★★★★');
+
+        // 방에 한 명의 플레이어민 남은 경우
+        // if ($scope.room.players.length == 1) {
+        //   $http.delete('api/rooms/' + $stateParams.roomId).then(function(response) {
+        //     console.log(response.data);
+        //     console.log('room is deleted');
+        //   });
+        // }
+
+        // 방에 여러 명의 플레이어가 남은 경우
+        // if ($scope.room.players.length > 1) {
+        //   $http.post('api/rooms/' + $scope.room.id + '/owner').then(function(response) {
+        //     console.log(response);
+        //   });
+        // }
 
         $http.put('/api/rooms/' + $stateParams.roomId, {
           status: $scope.room.status
@@ -142,8 +164,39 @@ angular.module('room', ['ionic'])
     };
 
     // 방에서 나가기
-    $scope.leaveRoom = function() {
+    $scope.leaveRoom = function(room) {
       console.log('leave room!');
+
+      // 플레이어가 한 명인 경우 나가기와 동시에 방 삭제
+      if (room.players.length == 1) {
+        $http.delete('api/rooms/' + room.id).then(function(response) {
+          console.log(response.data);
+          console.log('Room is deleted!');
+        });
+      }
+
+      // 플레이어가 두 명 이상인 경우 방장 위임하기 (break 기능 필요)
+      // if (room.players.length > 1) {
+      //   var newOwnerId = "";
+      //
+      //   angular.forEach(room.players, function(player) {
+      //     console.log("랜덤으로 선택된 플레이어 : " + player.userId);
+      //     console.log("방장 : " + room.ownerId);
+      //     console.log("로그인한 유저 : " + $scope.user._id);
+      //
+      //     // 로그인 유저와 오너가 동일하고(방장퇴장) 다른 플레이어이면 새로운 오너로 지정
+      //     if ($scope.user._id === room.ownerId && room.ownerId !== player.userId) {
+      //       newOwnerId = player.userId;
+      //       $http.post('api/rooms/' + room.id + '/owner', {
+      //         userId: newOwnerId
+      //       }).then(function(response) {
+      //         console.log(response);
+      //         console.log('OwnerId is changed');
+      //       });
+      //     }
+      //   });
+      // }
+
       window.location.href = '#/lobby';
     };
 
@@ -191,6 +244,7 @@ angular.module('room', ['ionic'])
         userId: '',
         content: $scope.players[msg.userId].username + '님이 퇴장하셨습니다.'
       });
+
       updateRoomInfo();
       $ionicScrollDelegate.$getByHandle('messages-scroll').scrollBottom(true);
     });
